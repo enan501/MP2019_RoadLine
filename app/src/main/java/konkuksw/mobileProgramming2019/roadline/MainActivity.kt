16957a784e6ea90ddbf010069402bcc1ae2ad4ac
enan <hant2258@gmail.com>
2019-05-14 00:54:13 +0900

    Initial commit

---
+++ app/src/main/java/konkuksw/mobileProgramming2019/roadline/MainActivity.kt
@@ -0,0 +1,13 @@
+package konkuksw.mobileProgramming2019.roadline
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //화이팅화이팅~~
+    }
+}
