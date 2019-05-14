9e89b574761825760f71beeb45142e297d14ac0e
gift_games <qudwns518@naver.com>
2019-05-14 20:48:14 +0900

    액티비티 생성

---
+++ app/src/main/java/konkukSW/MP2019/roadline/Date/AddDateActivity.kt
@@ -0,0 +1,13 @@
+package konkukSW.MP2019.roadline.Date
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import konkukSW.MP2019.roadline.R
+
+class AddDateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_date)
+    }
+}
--- app/src/main/java/konkukSW/MP2019/roadline/MainListActivity.kt
+++ app/src/main/java/konkukSW/MP2019/roadline/MainListActivity.kt
@@ -3,11 +3,11 @@ package konkukSW.MP2019.roadline
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_list)
         //화이팅화이팅~~
     }
 }
