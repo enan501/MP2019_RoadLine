9da471e84d388d03c999323ca791419233136a85
gift_games <qudwns518@naver.com>
2019-05-14 20:50:13 +0900

    액티비티 생성2

---
+++ app/src/main/java/konkukSW/MP2019/roadline/Money/AddMoneyActivity.kt
@@ -0,0 +1,13 @@
+package konkukSW.MP2019.roadline.Money
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import konkukSW.MP2019.roadline.R
+
+class AddMoneyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_money)
+    }
+}
---
+++ app/src/main/java/konkukSW/MP2019/roadline/Photo/ShowPhotoActivity.kt
@@ -0,0 +1,13 @@
+package konkukSW.MP2019.roadline.Photo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import konkukSW.MP2019.roadline.R
+
+class ShowPhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_photo)
+    }
+}
