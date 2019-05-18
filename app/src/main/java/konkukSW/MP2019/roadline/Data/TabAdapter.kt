812a34b7841ac6c1496723f5480ad3c299d91035
yeunjin1 <48876807+yeunjin1@users.noreply.github.com>
2019-05-18 18:47:15 +0900

    2019.05.18 showDate 업데이트

---
+++ app/src/main/java/konkukSW/MP2019/roadline/Data/TabAdapter.kt
@@ -0,0 +1,24 @@
+package konkukSW.MP2019.roadline.Data
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.ListFragment
+import konkukSW.MP2019.roadline.UI.date.*
+
+class TabAdapter(fm: FragmentManager, val num:Int): FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return num
+    }
+
+    override fun getItem(position: Int): Fragment? {
+        when(position){
+            0->return Fragment1()
+            1->return Fragment2()
+            2->return Fragment3()
+            3->return Fragment4()
+        }
+        return null
+    }
+}
