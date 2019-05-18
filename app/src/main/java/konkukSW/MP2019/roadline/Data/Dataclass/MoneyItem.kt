f47ad81cd127817469a2af08323169a52fc7ca40
gift_games <qudwns518@naver.com>
2019-05-18 14:43:50 +0900

    가계부 데이터/어댑터 클래스 추가

---
+++ app/src/main/java/konkukSW/MP2019/roadline/Data/Dataclass/MoneyItem.kt
@@ -0,0 +1,4 @@
+package konkukSW.MP2019.roadline.Data.Dataclass
+
+class MoneyItem {
+}
--- app/src/main/java/konkukSW/MP2019/roadline/UI/money/ShowMoneyActivity.kt
+++ app/src/main/java/konkukSW/MP2019/roadline/UI/money/ShowMoneyActivity.kt
@@ -2,10 +2,12 @@ package konkukSW.MP2019.roadline.UI.money
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import konkukSW.MP2019.roadline.Data.Adapter.MoneyItemAdapter
+import konkukSW.MP2019.roadline.Data.Dataclass.MoneyItem
 import konkukSW.MP2019.roadline.R
 
-var data:ArrayList<MyCafe> = ArrayList()
-lateinit var adapter:MyCafeAdapter
+var data:ArrayList<MoneyItem> = ArrayList()
+lateinit var adapter:MoneyItemAdapter
 
 class ShowMoneyActivity : AppCompatActivity() {
 
