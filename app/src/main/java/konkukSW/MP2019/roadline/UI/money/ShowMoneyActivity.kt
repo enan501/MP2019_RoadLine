1e11554b78c068d73b53525787c163f839ec386b
gift_games <qudwns518@naver.com>
2019-05-18 14:40:12 +0900

    ㅇㅇㅇㅇㅁㄴㅇ

--- app/src/main/java/konkukSW/MP2019/roadline/UI/money/ShowMoneyActivity.kt
+++ app/src/main/java/konkukSW/MP2019/roadline/UI/money/ShowMoneyActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import konkukSW.MP2019.roadline.R
 
+var data:ArrayList<MyCafe> = ArrayList()
+lateinit var adapter:MyCafeAdapter
+
 class ShowMoneyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
