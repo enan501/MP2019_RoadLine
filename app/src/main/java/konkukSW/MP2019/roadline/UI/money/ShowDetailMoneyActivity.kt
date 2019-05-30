62f00589766388c24e75e237149a18ae40b3c60d
shaelsien630 <shaelsien630@gmail.com>
2019-05-31 01:25:48 +0900

    ShowDetailMoneyActivity 추가

---
+++ app/src/main/java/konkukSW/MP2019/roadline/UI/money/ShowDetailMoneyActivity.kt
@@ -0,0 +1,13 @@
+package konkukSW.MP2019.roadline.UI.money
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import konkukSW.MP2019.roadline.R
+
+class ShowDetailMoneyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_detail_money)
+    }
+}
